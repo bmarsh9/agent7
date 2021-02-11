2c663d415540bc2829315c51b0c380b017550218
bmarsh9 <bmarshall735@gmail.com>
2021-02-11 13:41:43 -0500

    added white theme

--- app/local_settings.py
+++ app/local_settings.py
@@ -10,8 +10,31 @@ INITDBDIR = os.path.join(BASEDIR,"commands")
 # DO NOT use "DEBUG = True" in production environments
 DEBUG = True
 
+# DO NOT use Unsecure Secrets in production environments
+# Generate a safe one with:
+#     python -c "import os; print repr(os.urandom(24));"
+SECRET_KEY = 'This is an UNSECURE Secret. CHANGE THIS for production environments.'
+SITE_KEY = "737e079a-6170-4aae-91a6-60aca1f213aa"
+
+SERVER_HOST = "localhost"
+
+#// Customize
+COMPANY = "agent7"
+
+#// Postgres
+SQLALCHEMY_DATABASE_URI = 'postgresql://db1:db1@postgres_db/db1'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+RMQ_USER = "guest"
+RMQ_PASS = "guest"
+RMQ_HOST = "rabbitmq"
+RMQ_QUEUE = "agent7_queue"
+
 # Restricted fields from Models
 RESTRICTED_FIELDS = ["password"]
+
+# Theme color
+THEME_COLOR = os.environ.get("THEME_COLOR","dark") #dark or white
 
 # Default Valid RTR commands
 DEFAULT_CMD = {"commands": [
@@ -52,26 +75,6 @@ DEFAULT_JOB = {"jobset": [
         {"task":"set-localaccount","interval":600,"force":0,"enabled":0},
         {"task":"set-adaccount","interval":600,"force":0,"enabled":0},
 ]}
-
-# DO NOT use Unsecure Secrets in production environments
-# Generate a safe one with:
-#     python -c "import os; print repr(os.urandom(24));"
-SECRET_KEY = 'This is an UNSECURE Secret. CHANGE THIS for production environments.'
-SITE_KEY = "737e079a-6170-4aae-91a6-60aca1f213aa"
-
-SERVER_HOST = "localhost"
-
-#// Customize
-COMPANY = "agent7"
-
-#// Postgres
-SQLALCHEMY_DATABASE_URI = 'postgresql://db1:db1@postgres_db/db1'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-RMQ_USER = "guest"
-RMQ_PASS = "guest"
-RMQ_HOST = "rabbitmq"
-RMQ_QUEUE = "agent7_queue"
 
 # Logging Setup
 LOG_TYPE = "watched"  # Default is a Stream handler
