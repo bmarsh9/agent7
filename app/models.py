a49db215d4dbe2429756980ff0a18886c29d953a
bmarsh9 <bmarshall735@gmail.com>
2021-02-26 11:09:09 -0500

    Update models.py
    added agentscan and agentneighbor

--- app/models.py
+++ app/models.py
@@ -449,7 +449,7 @@ class AgentNeighbor(db.Model):
     mac = db.Column(db.String())
     type = db.Column(db.String())
     status = db.Column(db.String())
-    ports = db.relationship('NeighborPort', backref='neighbor', lazy='dynamic')
+    #ports = db.relationship('NeighborPort', backref='neighbor', lazy='dynamic')
     host_name = db.Column(db.String())    
     date_added = db.Column(db.DateTime, server_default=func.now())
     date_updated = db.Column(db.DateTime, onupdate=func.now())
@@ -457,15 +457,19 @@ class AgentNeighbor(db.Model):
     host_id = db.Column(db.Integer, db.ForeignKey('agent.id'), nullable=False)
     message_id = db.Column(db.String())    
 
-class NeighborPort(db.Model):
-    __tablename__ = "neighborport"
+class AgentScan(db.Model):
+    __tablename__ = "agentscan"
     id = db.Column(db.Integer, primary_key=True)
+    asset = db.Column(db.String())
+    address = db.Column(db.String())
+    mac = db.Column(db.String())
     port = db.Column(db.Integer)
     service = db.Column(db.String())
-    neighbor_id = db.Column(db.Integer, db.ForeignKey('agentneighbor.id'))
+    host_name = db.Column(db.String())        
     date_added = db.Column(db.DateTime, server_default=func.now())
     date_updated = db.Column(db.DateTime, onupdate=func.now())
     #// Ref
+    host_id = db.Column(db.Integer, db.ForeignKey('agent.id'), nullable=False)
     message_id = db.Column(db.String()) 
     
 class AgentMemory(db.Model):
